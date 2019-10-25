e10c0738af7f6f2a10cb007838aed089833e260f
JMGallego <jmgallego@ZiMac4-Master.local>
2019-10-25 21:04:23 +0200

    mvc not logic in the view

--- view/user.py
+++ view/user.py
@@ -1,17 +1,10 @@
 from controller.user import UserController
 
+user_controller = UserController()
 class UserView(object):
     def show_welcome(self):
         selection = int(input('Bienvenido, presiona 1 para logearte, si no tienes cuenta presiona 2 >  '))
-        if selection == 1:
-            self._log_in()
-        else:
-            self._sign_in()
+        user_controller.choose(selection)
         
-    def _log_in(self):
-        print('estoy en el log in')
-        
-    def _sign_in(self):
-        print('estoy en el SIGN in')
 
         
