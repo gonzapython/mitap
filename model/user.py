8a55258a3090b685efb94ec9ebbecb990990db1e
JMGallego <jmgallego@ZiMac4-Master.local>
2019-10-25 21:24:43 +0200

    add errors

--- model/user.py
+++ model/user.py
@@ -1,6 +1,16 @@
+from customerrors.user import PasswordsNotEqual
+
 class User(object):
     def log_in(self):
         print('estoy en el log in')
         
     def sign_in(self):
-        print('estoy en el SIGN in')
+        print('Has elegido registrarte')
+        username = input('Elige un nombre de usuario> ')
+        password = input('Elige una contraseña')
+        second_pass = input('Repite la contraseña')
+        self._check_pass(password, second_pass)
+    
+    def _check_pass(self, first, second):
+        if first != second:
+            raise PasswordsNotEqual
